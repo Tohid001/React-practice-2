6575249e1b509fc72b56d6a98563d23f16fcfc96
Tohidul Islam <mdtohid.sust@gmail.com>
2021-05-17 00:23:37 +0600

    correction of previous commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from "react";
 const initialState = 0;
-const reducer = (state, { type, value }) => {
+function reducer(state, { type, value }) {
   switch (type) {
     case "increment":
       return state + value;
@@ -10,7 +10,7 @@ const reducer = (state, { type, value }) => {
     default:
       return state;
   }
-};
+}
 function App() {
   const [count, dispatch] = useReducer(reducer, initialState);
   return (
@@ -19,25 +19,33 @@ function App() {
       <div>
         <button
           type="button"
-          onClick={dispatch({ type: "increment", value: 1 })}
+          onClick={() => {
+            dispatch({ type: "increment", value: 1 });
+          }}
         >
           increment by 1
         </button>
         <button
           type="button"
-          onClick={dispatch({ type: "decrement", value: 1 })}
+          onClick={() => {
+            dispatch({ type: "increment", value: 1 });
+          }}
         >
           decrement by 1
         </button>
         <button
           type="button"
-          onClick={dispatch({ type: "increment", value: 5 })}
+          onClick={() => {
+            dispatch({ type: "increment", value: 5 });
+          }}
         >
           increment by 5
         </button>
         <button
           type="button"
-          onClick={dispatch({ type: "decrement", value: 5 })}
+          onClick={() => {
+            dispatch({ type: "increment", value: 5 });
+          }}
         >
           decrement by 5
         </button>
